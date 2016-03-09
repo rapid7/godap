adbfe854a7b39358f92e696a835352269a28cd6e
Derek Abdine <dabdine@rapid7.com>
2016-03-09 02:09:35 -0800

    add json output

---
+++ output/json.go
@@ -0,0 +1,42 @@
+package output
+
+import (
+   "bufio"
+   "encoding/json"
+   "github.com/rapid7/godap/api"
+   "github.com/rapid7/godap/factory"
+)
+
+type OutputJson struct {
+   writer *bufio.Writer
+   FileDestination
+}
+
+func (oj *OutputJson) WriteRecord(data map[string]interface{}) {
+   json, err := json.Marshal(data)
+   if (err != nil) {
+      panic(err)
+   }
+   oj.writer.Write(json)
+   oj.writer.WriteString("\n")
+   oj.writer.Flush()
+}
+
+func (oj *OutputJson) Start() {
+}
+
+func (oj *OutputJson) Stop() {
+}
+
+func init() {
+   factory.RegisterOutput("json", func(args []string) (lines api.Output, err error) {
+      var file string
+      if (len(args) > 0) {
+         file = args[0]
+      }
+      outputJson := &OutputJson{}
+      err = outputJson.Open(file)
+      outputJson.writer = bufio.NewWriter(outputJson.fd)
+      return outputJson, nil
+   })
+}
