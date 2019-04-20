9286c1d857712587f9449aeaa3e9fa6b9eb22eda
Derek Abdine <dabdine@rapid7.com>
2019-04-19 19:50:41 -0700

    refactor geoip code to reuse for mmdb

---
+++ filter/geoip_legacy.go
@@ -0,0 +1,121 @@
+package filter
+
+import (
+	"errors"
+	"fmt"
+	"github.com/abh/geoip"
+	"github.com/rapid7/godap/api"
+	"github.com/rapid7/godap/factory"
+	"github.com/rapid7/godap/util"
+	"os"
+)
+
+func open_geoip_database(geoip_path string, default_database_files []string) (db *geoip.GeoIP, err error) {
+	stat, err := os.Stat(geoip_path)
+	if err != nil {
+		return nil, err
+	}
+
+	switch mode := stat.Mode(); {
+	case mode.IsDir():
+		for _, file := range default_database_files {
+			db, err = geoip.Open(fmt.Sprintf("%s/%s", geoip_path, file))
+			if err == nil {
+				break
+			}
+		}
+	case mode.IsRegular():
+		db, err = geoip.Open(geoip_path)
+	}
+
+	return
+}
+
+type GeoIPCityDecoder struct {
+	GeoIPDecoder
+	db *geoip.GeoIP
+}
+
+func NewGeoIPCityDecoder(db *geoip.GeoIP) (decoder *GeoIPCityDecoder, err error) {
+	decoder = new(GeoIPCityDecoder)
+
+	if db == nil {
+		return nil, errors.New("A geoip database must be supplied.")
+	}
+	decoder.db = db
+	return
+}
+
+func (g *GeoIPCityDecoder) decode(ip string, field string, doc map[string]interface{}) {
+	record := g.db.GetRecord(ip)
+	if record != nil {
+		doc[fmt.Sprintf("%s.country_code", field)] = record.CountryCode
+		doc[fmt.Sprintf("%s.country_code3", field)] = record.CountryCode3
+		doc[fmt.Sprintf("%s.country_name", field)] = record.CountryName
+		doc[fmt.Sprintf("%s.continent_code", field)] = record.ContinentCode
+		doc[fmt.Sprintf("%s.region", field)] = record.Region
+		doc[fmt.Sprintf("%s.city", field)] = record.City
+		doc[fmt.Sprintf("%s.postal_code", field)] = record.PostalCode
+		doc[fmt.Sprintf("%s.latitude", field)] = record.Latitude
+		doc[fmt.Sprintf("%s.longitude", field)] = record.Longitude
+		doc[fmt.Sprintf("%s.dma_code", field)] = record.MetroCode
+		doc[fmt.Sprintf("%s.area_code", field)] = record.AreaCode
+	}
+}
+
+func init() {
+	factory.RegisterFilter("geo_ip", func(args []string) (filterGeoIPCity api.Filter, err error) {
+		var db *geoip.GeoIP
+		if db, err = open_geoip_database(
+			util.GetEnv("GEOIP_CITY_DATABASE_PATH", "/var/lib/geoip"),
+			[]string{"geoip.dat", "geoip_city.dat", "GeoCity.dat", "IP_V4_CITY.dat", "GeoCityLite.dat"}); err != nil {
+			return nil, err
+		}
+
+		var decoder GeoIPDecoder
+		if decoder, err = NewGeoIPCityDecoder(db); err != nil {
+			return nil, err
+		}
+		return NewFilterGeoIP(args, decoder)
+	})
+}
+
+type GeoIPOrgDecoder struct {
+	GeoIPDecoder
+	db *geoip.GeoIP
+}
+
+func NewGeoIPOrgDecoder(db *geoip.GeoIP) (decoder *GeoIPOrgDecoder, err error) {
+	decoder = new(GeoIPOrgDecoder)
+
+	if db == nil {
+		return nil, errors.New("A geoip database must be supplied.")
+	}
+	decoder.db = db
+	return
+}
+
+func (g *GeoIPOrgDecoder) decode(ip string, field string, doc map[string]interface{}) {
+	record := g.db.GetOrg(ip)
+	if record != "" {
+		doc[fmt.Sprintf("%s.org", field)] = record
+	}
+}
+
+func init() {
+	factory.RegisterFilter("geo_ip_org", func(args []string) (filterGeoIPOrg api.Filter, err error) {
+		var db *geoip.GeoIP
+		if db, err = open_geoip_database(
+			util.GetEnv("GEOIP_ORG_DATABASE_PATH", "/var/lib/geoip"),
+			[]string{"geoip_org.dat", "IP_V4_ORG.dat"}); err != nil {
+			return nil, err
+		}
+
+		var decoder GeoIPDecoder
+		if decoder, err = NewGeoIPOrgDecoder(db); err != nil {
+			return nil, err
+		}
+
+		return NewFilterGeoIP(args, decoder)
+	})
+}
---
+++ filter/geoip_legacy_test.go
@@ -0,0 +1,190 @@
+package filter
+
+import (
+	"github.com/rapid7/godap/factory"
+	. "github.com/smartystreets/goconvey/convey"
+	"os"
+	"testing"
+)
+
+func TestGeoIPLegacyDatabase(t *testing.T) {
+	Convey("Given a non-existent path to the maxmind database", t, func() {
+		geoip_path := "/some/place/that/doesnt/exist"
+		default_database_files := make([]string, 0)
+
+		Convey("When the database is opened", func() {
+			filter, err := open_geoip_database(geoip_path, default_database_files)
+
+			Convey("An error is returned indicating the path doesn't exist", func() {
+				So(os.IsNotExist(err), ShouldBeTrue)
+			})
+
+			Convey("The filter should be nil", func() {
+				So(filter, ShouldBeNil)
+			})
+		})
+	})
+}
+
+func TestGeoIPCityDecoder(t *testing.T) {
+	Convey("Given an input ip address and geoip database", t, func() {
+		ip := "66.92.181.240" // test address in the GeoIPCity.dat test database
+		db, _ := open_geoip_database("../test/test_data/geoip/GeoIPCity.dat", []string{})
+		decoder, _ := NewGeoIPCityDecoder(db)
+
+		Convey("When the IP is processed by the decoder", func() {
+			result := make(map[string]interface{})
+			decoder.decode(ip, "line", result)
+
+			Convey("The result should have expected geo_ip fields", func() {
+				So(result, ShouldResemble, map[string]interface{}{
+					"line.country_code":   "US",
+					"line.country_code3":  "USA",
+					"line.country_name":   "United States",
+					"line.continent_code": "NA",
+					"line.region":         "CA",
+					"line.city":           "Fremont",
+					"line.postal_code":    "94538",
+					"line.latitude":       float32(37.5079),
+					"line.longitude":      float32(-121.96),
+					"line.dma_code":       807,
+					"line.area_code":      510,
+				})
+			})
+		})
+	})
+}
+
+func TestGeoIPCityFilterFactoryIntegration(t *testing.T) {
+	Convey("When a geo_ip (city) filter is created through the factory", t, func() {
+		os.Setenv("GEOIP_CITY_DATABASE_PATH", "../test/test_data/geoip/GeoIPCity.dat")
+		defer os.Unsetenv("GEOIP_CITY_DATABASE_PATH")
+		filterGeoIPCity, err := factory.CreateFilter([]string{"geo_ip", "line"})
+
+		Convey("The geo_ip filter should not be nil", func() {
+			So(filterGeoIPCity, ShouldNotBeNil)
+		})
+
+		Convey("The error should not be set (nil)", func() {
+			So(err, ShouldBeNil)
+		})
+
+		Convey("When the input is passed to the filter", func() {
+			in_doc := map[string]interface{}{"line": "66.92.181.240"}
+			result, err := filterGeoIPCity.Process(in_doc)
+
+			Convey("The result contains only one document", func() {
+				So(result, ShouldHaveLength, 1)
+			})
+
+			Convey("The first item in the result should have expected geo_ip fields", func() {
+				So(result[0], ShouldResemble, map[string]interface{}{
+					"line":                "66.92.181.240",
+					"line.country_code":   "US",
+					"line.country_code3":  "USA",
+					"line.country_name":   "United States",
+					"line.continent_code": "NA",
+					"line.region":         "CA",
+					"line.city":           "Fremont",
+					"line.postal_code":    "94538",
+					"line.latitude":       float32(37.5079),
+					"line.longitude":      float32(-121.96),
+					"line.dma_code":       807,
+					"line.area_code":      510,
+				})
+			})
+
+			Convey("The error should not be set", func() {
+				So(err, ShouldBeNil)
+			})
+		})
+	})
+
+	Convey("Given the GEOIP_CITY_DATABASE_PATH set to an invalid path", t, func() {
+		os.Setenv("GEOIP_CITY_DATABASE_PATH", "/does/not/exist")
+		defer os.Unsetenv("GEOIP_CITY_DATABASE_PATH")
+
+		Convey("When a geo_ip (city) filter is created", func() {
+			filterGeoIPCity, err := factory.CreateFilter([]string{"geo_ip", "line"})
+
+			Convey("The geo_ip filter should be nil", func() {
+				So(filterGeoIPCity, ShouldBeNil)
+			})
+
+			Convey("The error should indicate the directory does not exist", func() {
+				So(os.IsNotExist(err), ShouldBeTrue)
+			})
+		})
+	})
+}
+
+func TestGeoIPOrgDecoder(t *testing.T) {
+	Convey("Given an input ip address and geoip org database", t, func() {
+		ip := "12.87.118.0" // test address in the GeoIPOrg.dat test database
+		db, _ := open_geoip_database("../test/test_data/geoip/GeoIPOrg.dat", []string{})
+		decoder, _ := NewGeoIPOrgDecoder(db)
+
+		Convey("When the IP is processed by the decoder", func() {
+			result := make(map[string]interface{})
+			decoder.decode(ip, "line", result)
+
+			Convey("The result should have expected geo_ip_org fields", func() {
+				So(result, ShouldResemble, map[string]interface{}{
+					"line.org": "AT&T Worldnet Services",
+				})
+			})
+		})
+	})
+}
+
+func TestGeoIPOrgFilterFactoryIntegration(t *testing.T) {
+	Convey("When a geo_ip_org filter is created through the factory", t, func() {
+		os.Setenv("GEOIP_ORG_DATABASE_PATH", "../test/test_data/geoip/GeoIPOrg.dat")
+		defer os.Unsetenv("GEOIP_ORG_DATABASE_PATH")
+		filterGeoIPOrg, err := factory.CreateFilter([]string{"geo_ip_org", "line"})
+
+		Convey("The geo_ip_org filter should not be nil", func() {
+			So(filterGeoIPOrg, ShouldNotBeNil)
+		})
+
+		Convey("The error should not be set (nil)", func() {
+			So(err, ShouldBeNil)
+		})
+		Convey("When the input is passed to the filter", func() {
+			in_doc := map[string]interface{}{"line": "12.87.118.0"}
+			result, err := filterGeoIPOrg.Process(in_doc)
+
+			Convey("The result contains only one document", func() {
+				So(result, ShouldHaveLength, 1)
+			})
+
+			Convey("The first item in the result should have expected geo_ip_org fields", func() {
+				So(result[0], ShouldResemble, map[string]interface{}{
+					"line":     "12.87.118.0",
+					"line.org": "AT&T Worldnet Services",
+				})
+			})
+
+			Convey("The error should not be set", func() {
+				So(err, ShouldBeNil)
+			})
+		})
+	})
+
+	Convey("Given the GEOIP_ORG_DATABASE_PATH set to an invalid path", t, func() {
+		os.Setenv("GEOIP_ORG_DATABASE_PATH", "/does/not/exist")
+		defer os.Unsetenv("GEOIP_ORG_DATABASE_PATH")
+
+		Convey("When a geo_ip (city) filter is created", func() {
+			filterGeoIPOrg, err := factory.CreateFilter([]string{"geo_ip_org", "line"})
+
+			Convey("The geo_ip filter should be nil", func() {
+				So(filterGeoIPOrg, ShouldBeNil)
+			})
+
+			Convey("The error should indicate the directory does not exist", func() {
+				So(os.IsNotExist(err), ShouldBeTrue)
+			})
+		})
+	})
+}
